529cbfc30694270f856d44c0d7559fbbeff7d116
arifaulakh <aular@utschools.ca>
2017-11-16 21:13:58 -0500

    add solution

--- maximum_product.cpp
+++ maximum_product.cpp
@@ -3,48 +3,62 @@
 #include <algorithm>
 using namespace std;
 
-int maxSubarrayProduct(int arr[], int N)
-{
-   int max_ending_here = 1;
-
-   int min_ending_here = 1;
-   int max_so_far = 1;
-   for (int i = 0; i < N; i++)
-   {
-
-       if (arr[i] > 0)
-       {
-           max_ending_here = max_ending_here*arr[i];
-           min_ending_here = min (min_ending_here * arr[i], 1);
-       }
-
-       else if (arr[i] == 0)
-       {
-           max_ending_here = 1;
-           min_ending_here = 1;
-       }
-
-       else
-       {
-           int temp = max_ending_here;
-           max_ending_here = max (min_ending_here * arr[i], 1);
-           min_ending_here = temp * arr[i];
-       }
-
-       if (max_so_far <  max_ending_here)
-         max_so_far  =  max_ending_here;
-   }
-
-   return max_so_far;
-}
-
-int main()
-{
+int main(){
    int N; scanf("%d", &N);
    int arr[N];
+   
    for (int i = 0; i < N; i++){
        scanf("%d", &arr[i]);
    }
-   printf("%d\n", maxSubarrayProduct(arr, N));
+   sort(arr, arr+N);
+   int negcount = 0;
+   int total = 1;
+   bool isZero = true;
+   for (int i = 0; i <N; i++){
+     if (arr[i] == 0){
+       isZero = true;
+     }
+     else{
+       isZero = false;
+       break;
+     }
+   }
+   if (isZero){
+     total*=0;
+   }
+   else{
+     total = total;
+   }
+   for (int i = 0; i < N; i++){
+       if (arr[i] < 0){
+           negcount++;
+           if (N == 1){
+             total*= arr[i];
+           }
+       }
+       if (arr [i] > 0){
+           total*= arr[i];
+       }
+       if(arr[i] == 0){
+         
+           if ((arr[N-1] == 0 && negcount %2 != 0)|| N== 1 ){
+               total *= max(arr[i], arr[i-1]);
+           }
+           else{
+               total *=1;
+           }
+       }
+   }
+   if (negcount%2 == 0){
+       for (int i = 0; i < negcount; i++){
+           total*=arr[i];
+       }
+   }
+   if (negcount%2 != 0){
+       for (int i = 0; i < negcount-1; i++){
+           total*= arr[i];
+       }
+   }
+   printf("%d\n", total);
    return 0;
 }
