927351c57a1e00525a9cb1e0e8ebb37084a6f727
arifaulakh <aular@utschools.ca>
2018-12-11 00:14:21 -0500

    Update sieve.cpp

--- Algorithms/Math/sieve.cpp
+++ Algorithms/Math/sieve.cpp
@@ -3,7 +3,8 @@ using namespace std;
 const int MAXN = 1e6+6;
 int s[MAXN];
 void sieve(int n){
-    for (int x = 1; x<=n; x++){
+    s[1] = -1;
+    for (int x = 2; x<=n; x++){
         if (s[x])continue;
         for (int u = 2*x; u<=n; u+=x){
             s[u] = x;
