5e7b78faeca2fd231dc3b5b6bbd437780cbd6cf1
arifaulakh <aular@utschools.ca>
2019-01-29 11:03:57 -0500

    add LCS template/problem

--- Algorithms/Dynamic Programming/lcs.cpp
+++ Algorithms/Dynamic Programming/lcs.cpp
@@ -1,8 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXN = 1001;
-int N, M, X[MAXN], Y[MAXN], C[MAXN][MAXN];
+int N, M, C[MAXN][MAXN];
+string X[MAXN], Y[MAXN];
 
+string LCS(int i, int j){
+    if (i==0||j==0)return "";
+    if (X[i]==Y[j])return LCS(i-1,j-1)+X[i];
+    if (C[i][j-1]>C[i-1][j])return LCS(i,j-1);
+    return LCS(i-1,j);
+}
 int LCSlength(int n, int m){
 	for (int i = 0; i<=N; i++){
 		C[i][0] = 0;
