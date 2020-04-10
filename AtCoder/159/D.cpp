431f3555265e8bea76daff6813d7204750f3711d
arifaulakh <aular@utschools.ca>
2020-04-10 18:47:53 -0400

    add atcoder 158 D

--- AtCoder/159/D.cpp
+++ AtCoder/159/D.cpp
@@ -16,8 +16,9 @@ typedef map<int, int> mii;
 typedef map<ll, ll> mll;
 ll fpow(ll x, ll n, ll mod){if (n==0) return 1%mod; ll u = fpow(x, n/2, mod); u = (u*u)%mod; if (n%2==1)u = (u*x)%mod;return u;}
 ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
-
-int N, a[MAXN], freq[MAXN];
+ 
+ll N, a[MAXN], freq[MAXN];
+ 
 int main(){
     freopen("data.txt","r",stdin);
     cin >> N;
@@ -25,7 +26,19 @@ int main(){
         cin >> a[i];
         freq[a[i]]++;
     }
-    
-
+    ll total = 0;
+    for (int i = 1; i<=N; i++){
+      	if (freq[i] >= 2){
+          total  += ((freq[i])*(freq[i]-1))/2;
+        }
+    }
+    for (int i = 1; i<=N; i++){
+        ll temp = total;
+        temp -= ((freq[a[i]])*(freq[a[i]]-1))/2;
+        temp += ((freq[a[i]]-1)*(freq[a[i]]-2))/2;
+      	if (temp < 0) cout << 0 << endl;
+        else cout << temp << endl;
+    }
+ 
     return 0;
 }
